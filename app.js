4e34c2446ef3026763d1fb69e237ae6bb4ad6fad
Aldair Meraz <aldairrdz@gmail.com>
2022-01-13 19:48:41 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,106 @@
+//Exercise #1
+//Part 1
+/*
+var dayNumber = parseInt(prompt("Enter the day of the week in number format(Sunday = 0 - Saturday = 6)"));
+var hourNumber = parseInt(prompt("Enter only the hour(24h format)"));
+
+function businessHours(dayNumber, hourNumber){
+    switch (dayNumber){
+        case 0:
+            if (hourNumber>=9 && hourNumber<=18){
+                alert("Sunday at " + hourNumber +" hours. It's a business hour");
+                console.log(true);
+            } else if (hourNumber>0 && hourNumber<24){
+                alert("Sunday at " + hourNumber + " hours. It's not a business hour");
+                console.log(false);
+            } else {
+                alert("Incorrect value");
+                console.log("Incorrect value");
+            }
+            break;
+        case 1:
+            if (hourNumber>=9 && hourNumber<=18){
+                alert("Monday at " + hourNumber +" hours. It's a business hour");
+                console.log(true);
+            } else if (hourNumber>0 && hourNumber<24){
+                alert("Monday at " + hourNumber + " hours. It's not a business hour");
+                console.log(false);
+            } else {
+                alert("Incorrect value");
+                console.log("Incorrect value");
+            }
+            break;
+        case 2:
+            if (hourNumber>=9 && hourNumber<=18){
+                alert("Tuesday at " + hourNumber +" hours. It's a business hour");
+                console.log(true);
+            } else if (hourNumber>0 && hourNumber<24){
+                alert("Tuesday at " + hourNumber + " hours. It's not a business hour");
+                console.log(false);
+            } else {
+                alert("Incorrect value");
+                console.log("Incorrect value");
+            }
+            break;
+        case 3:
+            if (hourNumber>=9 && hourNumber<=18){
+                alert("Wednesday at " + hourNumber +" hours. It's a business hour");
+                console.log(true);
+            } else if (hourNumber>0 && hourNumber<24){
+                alert("Wednesday at " + hourNumber + " hours. It's not a business hour");
+                console.log(false);
+            } else {
+                alert("Incorrect value");
+                console.log("Incorrect value");
+            }
+            break;
+        case 4:
+            if (hourNumber>=9 && hourNumber<=18){
+                alert("Thursday at " + hourNumber +" hours. It's a business hour");
+                console.log(true);
+            } else if (hourNumber>0 && hourNumber<24){
+                alert("Thursday at " + hourNumber + " hours. It's not a business hour");
+                console.log(false);
+            } else {
+                alert("Incorrect value");
+                console.log("Incorrect value");
+            }
+            break;
+        case 5:
+            if (hourNumber>=9 && hourNumber<=18){
+                alert("Friday at " + hourNumber +" hours. It's a business hour");
+                console.log(true);
+            } else if (hourNumber>0 && hourNumber<24){
+                alert("Friday at " + hourNumber + " hours. It's not a business hour");
+                console.log(false);
+            } else {
+                alert("Incorrect value");
+                console.log("Incorrect value");
+            }
+            break;
+        case 6:
+            if (hourNumber>=9 && hourNumber<=18){
+                alert("Saturday at " + hourNumber +" hours. It's a business hour");
+                console.log(true);
+            } else if (hourNumber>0 && hourNumber<24){
+                alert("Saturday at " + hourNumber + " hours. It's not a business hour");
+                console.log(false);
+            } else {
+                alert("Incorrect value");
+                console.log("Incorrect value");
+            }
+            break;
+        default:
+            alert("Incorrect value");
+            console.log("Incorrect value");
+            break;
+    }
+}
+
+businessHours(dayNumber, hourNumber);
+*/
+//Part 2
+var janFirstDayNumber
+function getDayNumber(janFirstDayNumber, yearDayNumber){
+    
+}
